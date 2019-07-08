adee851d3d199de00b01c2916112de07da1ab469
Frank <frankamalafronte@gmail.com>
2019-07-07 23:51:31 -0400

    final draft lab

---
+++ src/cats.js
@@ -0,0 +1,29 @@
+const cats = [{
+  id:1,
+  name:'Taylor',
+  image:'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
+},
+{
+  id:2,
+  name:'Johnny',
+  image: 'https://cdn.pixabay.com/photo/2018/01/28/12/37/cat-3113513_1280.jpg'
+},
+{
+  id:3,
+  name:'Toby',
+  image: 'https://cdn.pixabay.com/photo/2014/05/07/06/44/animal-339400_1280.jpg'
+},
+{
+  id: 4,
+  name : 'Maggie',
+  image: 'https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_1280.jpg'
+},
+{
+  id:5,
+  name:'Mikey',
+  image:'https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_1280.jpg'
+}
+]
+
+
+module.exports = cats
---
+++ src/dogs.js
@@ -0,0 +1,25 @@
+const dogs = [{
+  id:1, 
+  name:'Tommy', 
+  image:"https://pixabay.com/photos/puppy-dog-pet-animal-cute-white-1903313/"
+},{
+  id:2, 
+  name: "Speedy",
+  image: "https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416__480.jpg"
+},
+{
+  id:3,
+  name: "Roger",
+  image: "https://cdn.pixabay.com/photo/2015/11/17/13/13/bulldog-1047518__480.jpg"
+},{
+  id: 4,
+  name: "Richard",
+  image: "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__480.jpg",
+  
+},{
+  id:5,
+  name:"Spot",
+  image: "https://cdn.pixabay.com/photo/2015/11/03/12/58/dog-1020790__480.jpg"
+}]
+
+module.exports = dogs
